b2d8e9cac931daa406b726b140e817684c6d904c
chlahfo <l830l@naver.com>
2023-02-03 09:17:15 +0900

    ㄱㄱ

--- python/01_baeckjun/01_01.py
+++ python/01_baeckjun/01_01.py
@@ -1,2 +1,11 @@
 #Hello world
 print("Hello World!")
+
+while True:
+    point = int(input("포인트 입력:"))
+    if point == 0:
+        break
+    num = point //10
+    for i in range(num):
+        print("*", end="")
+    print()
